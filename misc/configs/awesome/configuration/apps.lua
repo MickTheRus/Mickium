b30f78cbfe48bee8d96f5672cbca0465d1631e2b
Mick Romanchenko <mick.romanchenko@outlook.com>
2022-04-13 00:41:10 -0600

    Stuffium

--- misc/configs/awesome/configuration/apps.lua
+++ misc/configs/awesome/configuration/apps.lua
@@ -28,6 +28,7 @@ return {
     '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
     'xfce4-power-manager', -- Power manager
     'dunst -config ~/.config/awesome/configuration/dunstrc',
+    'flameshot', 
     '/usr/bin/variety',
     -- Add applications that need to be killed between reloads
     -- to avoid multipled instances, inside the awspawn script
