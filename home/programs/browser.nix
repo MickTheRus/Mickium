e26fb61fb5550a4d99e8014fa54763a1915e6c56
Nikolai Romanchenko <nikolai.romanchenko@proton.me>
2026-06-27 14:14:52 -0600

    Replace Tampermonkey with Violentmonkey

--- home/programs/browser.nix
+++ home/programs/browser.nix
@@ -7,14 +7,12 @@
       extensions = [
         {id = "dbepggeogbaibhgnhhndojpepiihcmeb";} # vimium
         {id = "eimadpbcbfnmbkopoojfekhnkhdbieeh";} # dark-reader
-        {id = "lljedihjnnjjefafchaljkhbpfhfkdic";} # jiffy reader
         {id = "mnjggcdmjocbbbhaepdhchncahnbgone";} # sponsorblock
         {id = "njdfdhgcmkocbgbhcioffdbicglldapd";} # LocalCDN
         {id = "icallnadddjmdinamnolclfjanhfoafe";} # FastForward
         {id = "fkagelmloambgokoeokbpihmgpkbgbfm";} # Indie Wiki Buddy
         {id = "ghmbeldphafepmbegfdlkpapadhbakde";} # Proton Pass
-        {id = "dhdgffkkebhmkfjojejmpbldmpobfkfo";} # Tampermonkey
-        {id = "bnomihfieiccainjcjblhegjgglakjdd";} # Improve Youtube
+        {id = "jinjaccalgkegednnccohejagnlnfdag";} # Violentmonkey
       ];
     };
   };
