da80a7d6e13da4cc576239c15fca285facb7f327
Nikolai Romanchenko <nikolai.romanchenko@proton.me>
2026-06-30 14:32:40 -0600

    Fix GTK Catppuccin theme

--- home/programs/gtk.nix
+++ home/programs/gtk.nix
@@ -2,26 +2,45 @@
   pkgs,
   config,
   ...
-}: {
+}: let
+  gtkThemeName = "catppuccin-mocha-maroon-standard";
+  gtkTheme = pkgs.catppuccin-gtk.override {
+    accents = ["maroon"];
+    size = "standard";
+    variant = "mocha";
+  };
+  iconThemeName = "candy-icons";
+  cursorThemeName = "Adwaita";
+in {
   # cursor theme
   home.pointerCursor = {
-    name = "Adwaita";
+    name = cursorThemeName;
     package = pkgs.adwaita-icon-theme;
     size = 24;
     gtk.enable = true;
   };
 
+  home.sessionVariables.GTK_THEME = gtkThemeName;
+
+  dconf.settings."org/gnome/desktop/interface" = {
+    color-scheme = "prefer-dark";
+    cursor-theme = cursorThemeName;
+    font-name = "Iosevka Nerd Font 11";
+    gtk-theme = gtkThemeName;
+    icon-theme = iconThemeName;
+  };
+
   gtk = {
     enable = true;
     theme = {
-      name = "catppuccin-mocha-maroon-standard+default";
-      package = pkgs.catppuccin-gtk;
+      name = gtkThemeName;
+      package = gtkTheme;
     };
 
     gtk4.theme = config.gtk.theme;
 
     iconTheme = {
-      name = "candy-icons";
+      name = iconThemeName;
       package = pkgs.candy-icons;
     };
 
